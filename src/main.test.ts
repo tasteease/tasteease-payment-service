cdcf476a573eb76f7be3dcf32b4126cc31237f81
José Ivan R. de Oliveira <ivan_r.o@hotmail.com>
2024-05-12 17:16:07 -0300

    feat: creates main test file

---
+++ src/main.test.ts
@@ -0,0 +1,21 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { bootstrap } from './main';
+
+jest.mock('@nestjs/core', () => ({
+  NestFactory: {
+    create: jest.fn(() => ({
+      listen: jest.fn(),
+    })),
+  },
+}));
+
+describe('main', () => {
+  describe('bootstrap', () => {
+    it('should create an app and listen on port 3000', async () => {
+      await bootstrap();
+
+      expect(NestFactory.create).toHaveBeenCalledWith(AppModule);
+    });
+  });
+});
